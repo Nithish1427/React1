2f76af072a0de711482b8da1895afafc66dd9642
Nithish <nithishrajaduraip@gmail.com>
2025-12-03 20:44:26 +0530

    onChange Event Handling - 1 (Function SetUp & Check)

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,18 +1,47 @@
+import { useState } from "react";
+
 function SignUp() {
+    
+  const [pwd1, setPwd1] = useState("");
+  const [pwd2, setPwd2] = useState("");
+
+  function handlePwd1Change(event) {
+    setPwd1(event.target.value);
+    console.log(event.target.value);
+  }
+
+  function handlePwd2Change(event) {
+    setPwd2(event.target.value);
+    console.log(event.target.value);
+    console.log(pwd1, pwd2); 
+    // setPwd1 and setPwd2 are asynchronous functions - don't happen simultaneously
+    // hence when printed like this, the last entry of pwd2 will be missing
+  }
+
   return (
     <>
-      <form className="my-5" style={{width:"50%", margin:"auto"}}>
+      <form className="my-5" style={{ width: "50%", margin: "auto" }}>
         <div className="mb-3">
           <label className="form-label">Email address</label>
           <input type="email" className="form-control" />
         </div>
         <div className="mb-3">
           <label className="form-label">Password</label>
-          <input type="password" className="form-control" />
+          <input
+            value={pwd1}
+            onChange={handlePwd1Change}
+            type="password"
+            className="form-control"
+          />
         </div>
         <div className="mb-3">
           <label className="form-label">Re-Enter Password</label>
-          <input type="password" className="form-control" />
+          <input
+            value={pwd2}
+            onChange={handlePwd2Change}
+            type="password"
+            className="form-control"
+          />
         </div>
         <div className="mb-3 form-check">
           <input type="checkbox" className="form-check-input" />

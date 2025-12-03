78e298d7c649a809de19c43c2e5cbf6d4c0314bf
Nithish <nithishrajaduraip@gmail.com>
2025-12-03 21:05:30 +0530

    onChange Event Handling - 2 (PwdCheck at CheckBox)

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -5,17 +5,25 @@ function SignUp() {
   const [pwd1, setPwd1] = useState("");
   const [pwd2, setPwd2] = useState("");
 
+  const [same, setSame] = useState(true);
+
   function handlePwd1Change(event) {
     setPwd1(event.target.value);
-    console.log(event.target.value);
   }
 
   function handlePwd2Change(event) {
     setPwd2(event.target.value);
-    console.log(event.target.value);
-    console.log(pwd1, pwd2); 
-    // setPwd1 and setPwd2 are asynchronous functions - don't happen simultaneously
-    // hence when printed like this, the last entry of pwd2 will be missing
+  }
+
+  function handlePwdCheck() {
+    if(pwd1==pwd2) {
+        console.log("Same");
+        setSame(true);
+    }
+    else {
+        console.log("Different");
+        setSame(false);
+    }
   }
 
   return (
@@ -44,9 +52,10 @@ function SignUp() {
           />
         </div>
         <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" />
+          <input onChange={handlePwdCheck} type="checkbox" className="form-check-input" />
           <label className="form-check-label"> I Agree </label>
         </div>
+        {!same && <p>Passwords do not match</p>}
         <button type="submit" className="btn btn-primary">
           Create Account
         </button>

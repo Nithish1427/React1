96aa890597fed948ddfc95fa869391ddc74ccbdf
Nithish <nithishrajaduraip@gmail.com>
2025-12-04 09:59:07 +0530

    React Router - Navigating through Home, SignUp and PageNotFound

---
+++ src/PageNotFound.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function PageNotFound() {
+  return (
+    <>
+      <p>Error: 404 - Page Not Found</p>
+      <a className="btn btn-primary" href="Home">Home</a>
+      {/* this sends request to server each time - activity in network tab - needs internet connection - doesn't work offline*/}
+      {/* advantage of React - Single Page Application - no need to go to the server every time - so, using <a></a> tag is of no use here */}
+      <Link className="btn btn-danger" to="/">Home</Link>
+      {/* using <Link></Link> tag - routing happens within the page - not unnecessarily requesting server - only for necessity like fetching data from an API Endpoint */}
+    </>
+  );
+}
+
+export default PageNotFound
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Login from './Login.jsx'
 import SignUp from './SignUp.jsx'
+import Home from './Home.jsx'
+import PageNotFound from './PageNotFound.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Home />,
+    errorElement : <PageNotFound />
+  },
+  {
+    path : '/SignUp',
+    element : <SignUp />,
+    errorElement : <PageNotFound />
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SignUp />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
+
+// // for installing router
+// npm install react-router-dom
